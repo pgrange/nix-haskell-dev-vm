67834d9d97c3fc431affdda79cc5def3faa860a0
Arnaud Bailly <arnaud.bailly@iohk.io>
2021-04-21 08:48:26 +0200

    Fix snapshot script to be more general

--- scripts/snapshot.sh
+++ scripts/snapshot.sh
@@ -3,14 +3,18 @@
 # This first deletes the previous snapshot, which might not be a great idea.
 # Should probably have rolling upgrades?
 
-SNAPSHOT_NAME=haskell-dev-vm-snapshot
-DISK_NAME=haskell-dev-vm-disk
-PROJECT=$(terraform output -raw project)
+SNAPSHOT_NAME=iog-hydra-dev-vm-snapshot
+BASE_DISK_NAME=haskell-dev-vm-disk
+
+# extract project name from terraform's output or environment
+PROJECT=${PROJECT:-$(terraform output -raw project)}
 
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 
+DISK_NAME=$(gcloud --project ${PROJECT} compute disks list | grep $BASE_DISK_NAME | head | cut -d ' ' -f1)
+
 # Ensure disk exists before creating snapshot
-if gcloud "--project=${PROJECT}" compute disks list | grep $DISK_NAME ; then
+if ! [ -z ${DISK_NAME} ] ; then
     gcloud "--project=${PROJECT}" compute snapshots delete $SNAPSHOT_NAME
     gcloud "--project=${PROJECT}" compute disks snapshot $DISK_NAME --snapshot-names=$SNAPSHOT_NAME  --description="Snapshot - $(date "+%Y-%m-%d")" --zone europe-west4-b
 fi
