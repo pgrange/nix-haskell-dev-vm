978997b531e09ff6899de5d815f036119b5474fc
Pascal Grange <pascal@grange.nom.fr>
2022-10-25 16:48:56 +0200

    FIX forgot to allow egress connections

--- AWS/firewall.tf
+++ AWS/firewall.tf
@@ -10,5 +10,13 @@ resource "aws_security_group" "haskell-dev-vm" {
       "0.0.0.0/0"
     ]
   }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
 
