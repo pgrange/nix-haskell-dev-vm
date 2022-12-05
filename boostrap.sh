a7916c468c8f250bba55873105477ac4051f0605
Pascal Grange <pascal@grange.nom.fr>
2022-12-05 17:52:20 +0100

    Restart nix service when changing configuration

--- boostrap.sh
+++ boostrap.sh
@@ -70,6 +70,7 @@ substituters = https://cache.nixos.org https://hydra.iohk.io https://iohk.cachix
 trusted-public-keys = iohk.cachix.org-1:DpRUyj7h7V830dp/i6Nti+NEO2/nhblbov/8MW7Rqoo= hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ= cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
 experimental-features = nix-command flakes
 EOF
+sudo systemctl restart nix-daemon.service
 
 # install direnv
 sudo apt install -y direnv
